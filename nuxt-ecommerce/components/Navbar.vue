20808137bae59550fb85182f7de0f7a88bf783d7
martinDTV <martin.delatorre@monterrey.gob.mx>
2025-03-03 19:23:15 -0600

    login con keycloak y discord

--- nuxt-ecommerce/components/Navbar.vue
+++ nuxt-ecommerce/components/Navbar.vue
@@ -1,15 +1,32 @@
 <template>
   <header class="h-[80px] flex justify-center items-center p-4">
     <div class="w-full flex justify-end gap-4">
-      <UButton color="white" variant="outline" target="Carrito">
-        <UIcon name="mdi:menu" class="w-5 h-5" />
-      </UButton>
+      <UDropdown v-model:open="open" :items="items" :popper="{ placement: 'bottom-start' }">
+        <UButton color="white" trailing-icon="mdi:menu" />
+      </UDropdown>
     </div>
   </header>
 </template>
 
 <script setup lang="ts">
+import { ref,watch } from "vue"
 const config = useRuntimeConfig();
+import {userStore} from "~/stores/userStore";
+
+const open = ref(false)
+const useUserStore = userStore()
+
+const items = ref([
+  [{
+    label: useUserStore.auth.token !== '' ? 'Cerrar sesion' : 'Iniciar Sesion',
+    click: () => {
+      useUserStore.auth.token === '' ? loginKeycloak() : () => {
+        useUserStore.clearAuth()
+        window.location.reload()
+      }
+    }
+  }]
+])
 
 // Redirige a Keycloak para iniciar sesión
 const loginDiscord = () => {
